6da00b9e69256292158a9b907838fd8110ada28f
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2023-06-30 17:13:14 +0200

    Add gh pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/easyreach-2023/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
